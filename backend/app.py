a5183cec49e8cad10840b5d9bcd462d1c19dba41
Sami <hellosami@protonmail.com>
2024-04-25 09:40:49 +0200

    Allow CORS in backend for API requests

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import sys
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.users import router as users_router
 
 # Create an instance of the FastAPI class
@@ -7,6 +8,21 @@ app = FastAPI()
 
 # Include routers
 app.include_router(users_router)
+
+# Define allowed origins
+allowed_origins = [
+    "http://localhost:3000",
+    "http://example.com",
+]
+
+# Allow CORS and requests from defined origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 test = {"id": "1234", "name": "bagarsmurfen"}
 
