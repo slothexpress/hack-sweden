495ec210a636d11d8638ee5db1076463d4506725
nordenek <elsa.norden@outlook.com>
2024-04-27 15:36:02 +0200

    Add test data for transportation and pop-up.

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -4,20 +4,16 @@ import {
   TileLayer,
   LayerGroup,
   CircleMarker,
+  Popup,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import {stations} from "./stationsTestData"
 
 const Map = () => {
   const mapRef = useRef(null);
   const latitude = 55.6918412;
   const longitude = 13.3603703;
-  const busStops = [
-    [55.695, 13.36038],
-    [55.6966, 13.36038],
-    [55.697, 13.36038],
-    [55.698, 13.36038],
-  ];
-
+  
   return (
     // Make sure you set the height and width of the map container otherwise the map won't show
     <MapContainer
@@ -27,12 +23,14 @@ const Map = () => {
       style={{ height: "50vh", width: "50vw" }}
     >
       <LayerGroup>
-        {busStops.map((coordinates) => (
+        {stations.map((station) => (
           <CircleMarker
-            center={coordinates}
+            center={[station.coordinates[1], station.coordinates[0]]}
             radius={5}
             pathOptions={{ color: "red" }}
-          ></CircleMarker>
+          >
+            <Popup>{station.name}</Popup>
+          </CircleMarker>
         ))}
       </LayerGroup>
       <TileLayer
