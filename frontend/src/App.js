679ec601b77a10e44fe2d1bb272f969b26e46c76
Aleksandra Magdziarek <aleksandra.magdziarek@trialbee.com>
2024-04-27 13:41:23 +0200

    feat: add leaflet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const API_URL = "http://localhost";
@@ -25,18 +24,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <div>
           <h1>Data from Backend API:</h1>
           <pre>{JSON.stringify(data, null, 2)}</pre>
