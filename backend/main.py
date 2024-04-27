822adde6b5fb587d28927b7146a2cc2f29f4adce
Sami <hellosami@protonmail.com>
2024-04-27 11:47:07 +0200

    Refactor: clean up code

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-import sys
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.root import router as root_router
@@ -7,10 +6,10 @@ from routers.root import router as root_router
 # Create an instance of the FastAPI class
 app = FastAPI()
 
-# Include routers
+# Include root router only!
 app.include_router(root_router)
 
-# Define allowed origins
+# TODO: Define allowed origins
 allowed_origins = [
     "http://localhost:3000",
     "http://example.com",
@@ -27,9 +26,3 @@ app.add_middleware(
 
 #### TEST DATA STUFF HERE #####
 
-
-# Run the FastAPI application using Uvicorn
-#if __name__ == "__main__":
-    #import uvicorn
-    #uvicorn.run(app, host="localhost", port=8000)
-    
